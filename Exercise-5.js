6986bcd46f7071b5d05970b5020a47f33baceb42
darindra23 <58590046+darindra23@users.noreply.github.com>
2019-12-16 18:13:37 +0700

    Add files via upload

---
+++ Exercise-5.js
@@ -0,0 +1,40 @@
+// SOAL NOMOR 1
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console.log(word + ' ' + second + ' ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth + ' ' + seventh)
+
+// SOAL NOMOR 2
+
+var word = 'wow JavaScript is so cool';
+var firstword = word[0] + word [1] + word[2];
+var secondword = word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word [12] + word [13]
+var thirdword = word[15] + word[16] 
+var fourthword = word[18] + word[19]
+var fifthword = word[21] + word[22] + word[23] + word[24]
+
+console.log('First Word: ' + firstword);
+console.log('Second Word: ' + secondword);
+console.log('Third Word: ' + thirdword);
+console.log('Fourth Word: ' + fourthword);
+console.log('Fifth Word: ' + fifthword)
+
+// SOAL NOMOR 3
+
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14);
+var thirdWord3 = word.substring(15, 17);
+var fourthWord3 = word.substring(18, 20);
+var fifthWord3 = word.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
