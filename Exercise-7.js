0d4a5752ea52c0b217ef76a82e54c484197093b9
darindra23 <58590046+darindra23@users.noreply.github.com>
2019-12-17 11:46:36 +0700

    Add files via upload

---
+++ Exercise-7.js
@@ -0,0 +1,28 @@
+// SOAL NOMOR 1
+var rows1 = 5
+
+for(i = 0; i <= rows1; i++){
+    console.log('*')
+}
+
+// SOAL NOMOR 2
+var rows2 = 5
+
+for(i = 0; i <= rows2; i++){
+    y = ''
+    for(j = 1; j <=rows2; j++){
+        y = y + '*'
+    }
+    console.log(y)
+}
+
+
+// SOAL NOMOR 3
+var rows3 = 5;
+var x = ''
+for(i = 0; i < rows3 ; i++) {
+    for(j = 0; j <= i; j++){
+    }
+    x += '*'
+    console.log(x)
+}  
