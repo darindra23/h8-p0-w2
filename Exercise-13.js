9a15344ba8b6a9e842f75fed515b2fef7eba4458
darindra23 <58590046+darindra23@users.noreply.github.com>
2019-12-18 15:58:35 +0700

    Create Exercise-13.js

---
+++ Exercise-13.js
@@ -0,0 +1,25 @@
+function xo(str) {
+    let x = 0;
+    let o = 0;
+    
+
+    for(i = 0; i < str.length; i++){
+        if( str.charAt(i) === 'x'){
+            x++
+        } else{
+            o++
+        }
+    }
+    if( x === o){
+        return true
+    } else {
+        return false
+    }
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
